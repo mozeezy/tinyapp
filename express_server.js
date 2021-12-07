45d8dcfcce8fb26078feb4411bab9da3fed7fa87
Mohamed Mohamed <justmoe23@gmail.com>
2021-12-07 00:43:53 +0000

    shortening url pt. 1

--- express_server.js
+++ express_server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+
+}
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.set("view engine", "ejs");
 
@@ -12,6 +23,10 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
